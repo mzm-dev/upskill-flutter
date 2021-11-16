ccf1a7e802be7b1376a0cf123ec6aa45f120b049
Mohamad Zaki Mustafa <mohamadzakimustafa@gmail.com>
2021-11-16 12:50:30 +0800

    Update home.dart

--- flutter_projek/flutter_application_4/lib/pages/home.dart
+++ flutter_projek/flutter_application_4/lib/pages/home.dart
@@ -14,151 +14,113 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text('Material App Bar'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            padding: EdgeInsets.all(5),
-            height: 150,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('images/fruit/fruit.jpg'),
-                    fit: BoxFit.cover)),
-            //color: Colors.orange,
-            //child: Text('Column 2'),
-          ),
-          Container(
-            padding: EdgeInsets.all(5),
-            height: 80,
-            alignment: Alignment.centerRight,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('images/fruit/fruit_main.jpg'),
-                    fit: BoxFit.cover)),
-            //color: Colors.orange,
-            child: const Text('My Fruits',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                    letterSpacing: 3.5,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Bubblegum')),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(0, 20, 0, 10),
-            child: Text('Column 3'),
-          ),
-          Container(
-            child: Row(
-              children: [
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                ShowPage(gambar: 'fruit_1.jpg')));
-                  },
-                  child: Container(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              padding: EdgeInsets.all(5),
+              height: 150,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('images/fruit/fruit.jpg'),
+                      fit: BoxFit.cover)),
+              //color: Colors.orange,
+              //child: Text('Column 2'),
+            ),
+            Container(
+              padding: EdgeInsets.all(5),
+              height: 80,
+              alignment: Alignment.centerRight,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('images/fruit/fruit_main.jpg'),
+                      fit: BoxFit.cover)),
+              //color: Colors.orange,
+              child: const Text('My Fruits',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      letterSpacing: 3.5,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Bubblegum')),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(0, 20, 0, 10),
+              child: Text('Column 3'),
+            ),
+            Container(
+              child: Row(
+                children: [
+                  KlikGambarWidget(title: 'Fruit 1', nama: 'fruit_1.jpg'),
+                  KlikGambarWidget(title: 'Fruit 2', nama: 'fruit_2.jpg'),
+                  KlikGambarWidget(title: 'Fruit 3', nama: 'fruit_3.jpg'),
+                  KlikGambarWidget(title: 'Fruit 4', nama: 'fruit_4.jpg'),
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              child: Row(
+                children: [
+                  Container(
                     height: 100,
                     width: MediaQuery.of(context).size.width * 0.25,
                     decoration: BoxDecoration(
                         image: DecorationImage(
-                            image: AssetImage('images/fruit/fruit_1.jpg'),
+                            image: AssetImage('images/fruit/fruit_5.jpg'),
                             fit: BoxFit.cover)),
-                    //color: Colors.purple[100],
+                    //color: Colors.teal[400],
                     //child: Center(child: Text('Row 1')),
                   ),
-                ),
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                ShowPage(gambar: 'fruit_2.jpg')));
-                  },
-                  child: Container(
+                  Container(
                     height: 100,
-                    width: MediaQuery.of(context).size.width * 0.25,
+                    width: MediaQuery.of(context).size.width * 0.75,
                     decoration: BoxDecoration(
                         image: DecorationImage(
-                            image: AssetImage('images/fruit/fruit_2.jpg'),
+                            alignment: Alignment.topCenter,
+                            image: AssetImage('images/fruit/fruit_6.jpg'),
                             fit: BoxFit.cover)),
-                    //color: Colors.purple[200],
+                    //color: Colors.teal[200],
                     //child: Center(child: Text('Row 2')),
                   ),
-                ),
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                ShowPage(gambar: 'fruit_3.jpg')));
-                  },
-                  child: Container(
-                    height: 100,
-                    width: MediaQuery.of(context).size.width * 0.25,
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage('images/fruit/fruit_3.jpg'),
-                            fit: BoxFit.cover)),
-                    //color: Colors.purple[300],
-                    //child: Center(child: Text('Row 3')),
-                  ),
-                ),
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                ShowPage(gambar: 'fruit_4.jpg')));
-                  },
-                  child: Container(
-                    height: 100,
-                    width: MediaQuery.of(context).size.width * 0.25,
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage('images/fruit/fruit_4.jpg'),
-                            fit: BoxFit.cover)),
-                    //color: Colors.purple[400],
-                    //child: Center(child: Text('Row 4')),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          SizedBox(height: 10),
-          Container(
-            child: Row(
-              children: [
-                Container(
-                  height: 100,
-                  width: MediaQuery.of(context).size.width * 0.25,
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage('images/fruit/fruit_5.jpg'),
-                          fit: BoxFit.cover)),
-                  //color: Colors.teal[400],
-                  //child: Center(child: Text('Row 1')),
-                ),
-                Container(
-                  height: 100,
-                  width: MediaQuery.of(context).size.width * 0.75,
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          alignment: Alignment.topCenter,
-                          image: AssetImage('images/fruit/fruit_6.jpg'),
-                          fit: BoxFit.cover)),
-                  //color: Colors.teal[200],
-                  //child: Center(child: Text('Row 2')),
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class KlikGambarWidget extends StatelessWidget {
+  KlikGambarWidget({
+    Key? key,
+    required this.title,
+    required this.nama,
+  }) : super(key: key);
+
+  String title;
+  String nama;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => ShowPage(gambar: nama, title: title)));
+      },
+      child: Container(
+        height: 100,
+        width: MediaQuery.of(context).size.width * 0.25,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('images/fruit/$nama'), fit: BoxFit.cover)),
+        //color: Colors.purple[100],
+        //child: Center(child: Text('Row 1')),
       ),
     );
   }
